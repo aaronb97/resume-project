b2b4f769a9529155da25e631fda40b061191d05b
aaronb97 <aaron9becker7@gmail.com>
2025-04-13 15:43:14 -0400

    create api directory

---
+++ api/src/ResumeAPI.API/Program.cs
@@ -0,0 +1,71 @@
+using Amazon;
+using Amazon.S3;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using ResumeAPI;
+using ResumeAPI.Endpoints;
+using ResumeAPI.Models;
+using ResumeAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAWSService<IAmazonS3>();
+builder.Services.Configure<S3Settings>(builder.Configuration.GetSection("S3Settings"));
+
+builder.Services.AddSingleton<IAmazonS3>(sp =>
+{
+    var s3Settings = sp.GetRequiredService<IOptions<S3Settings>>().Value;
+    var config = new AmazonS3Config()
+    {
+        RegionEndpoint = RegionEndpoint.GetBySystemName(s3Settings.Region),
+    };
+
+    return new AmazonS3Client(config);
+});
+
+builder.Services.AddSingleton<AiService>();
+
+var connectionString = builder.Configuration.GetConnectionString("PostgresConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc(
+            "v1",
+            new OpenApiInfo
+            {
+                Title = "Todo API",
+                Description = "Keep track of your tasks",
+                Version = "v1",
+            }
+        );
+    });
+}
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+    });
+}
+
+app.MapDocxUpload();
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
