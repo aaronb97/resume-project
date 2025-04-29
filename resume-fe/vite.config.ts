f8cc5948bf453d0be04b353f74c16e5d45dd6b74
aaronb97 <aaron9becker7@gmail.com>
2025-04-29 10:33:43 -0400

    update ts router, use code splitting

--- resume-fe/vite.config.ts
+++ resume-fe/vite.config.ts
@@ -6,7 +6,11 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ target: "react" }), tailwindcss(), react()],
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    tailwindcss(),
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
